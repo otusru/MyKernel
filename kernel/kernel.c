6b4b92fcf19d27a5694b6051d34ac07bc7522550
otusru <otusru@mail.ru>
2025-05-29 19:29:05 +0300

    Create kernel.c

---
+++ kernel/kernel.c
@@ -0,0 +1,22 @@
+#include "kernel.h"
+#include "memory.h"
+#include "scheduler.h"
+#include "interrupt.h"
+#include "keyboard.h"
+#include "timer.h"
+
+void kernel_main(void) {
+    // Инициализация подсистем
+    init_memory();
+    init_interrupts();
+    init_scheduler();
+    init_keyboard();
+    init_timer();
+
+    // Создание задач
+    create_task(task1);
+    create_task(task2);
+
+    // Запуск планировщика
+    start_scheduler();
+}
