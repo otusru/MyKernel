225fc2f81ad91c64af01883bf32f85132ddd543e
otusru <otusru@mail.ru>
2025-05-29 20:01:33 +0300

    Update string.c

--- lib/string.c
+++ lib/string.c
@@ -1,1 +1,7 @@
+void *memcpy(void *dest, const void *src, unsigned long n) {
+    char *d = dest;
+    const char *s = src;
+    for (unsigned long i = 0; i < n; i++) d[i] = s[i];
+    return dest;
+}
 
