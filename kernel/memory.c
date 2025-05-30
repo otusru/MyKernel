688de0ca136b6d38989f0d42ae30caee99b77e2f
otusru <otusru@mail.ru>
2025-05-30 11:00:32 +0300

    Update memory.c

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,1 +1,8 @@
+// Заглушка для управления памятью
+// kernel/memory.c
+#include "memory.h"
+#include "malloc.h"
 
+void init_memory() {
+    // Пока что логика отсутствует — можно подключать malloc
+}
