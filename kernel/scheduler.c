2cf4b34be0a033801bad43da28bc54cab5af38d4
otusru <otusru@mail.ru>
2025-05-30 10:56:32 +0300

    Update scheduler.c

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -1,31 +1,3 @@
-/*#include "scheduler.h"
-
-static task_t *current_task = NULL;
-static task_t *task_queue = NULL;
-
-void init_scheduler(void) {
-    // Инициализация планировщика
-}
-
-void create_task(void (*task_func)(void)) {
-    // Создание новой задачи
-}
-
-void schedule(void) {
-    // Выбор следующей задачи для выполнения
-    current_task = task_queue;
-    // Переключение контекста
-}
-
-void init_scheduler() {
-    // Заглушка: Инициализация очереди задач
-}
-
-void start_scheduler() {
-    // Заглушка: Запуск задач
-}
-*/
-
 #include "scheduler.h"
 
 #define MAX_TASKS 10
