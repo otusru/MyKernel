1cc443f47d14288be3e427bc37c52b92a5c35357
otusru <otusru@mail.ru>
2025-05-30 10:42:25 +0300

    Update keyboard.c

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -1,6 +1,13 @@
 #include "keyboard.h"
+#include "io.h"
+
+#define KEYBOARD_DATA_PORT 0x60
 
 void init_keyboard() {
+    // Здесь можно настроить IRQ1
     // Заглушка: Инициализация клавиатуры
 }
 
+unsigned char keyboard_read() {
+    return inb(KEYBOARD_DATA_PORT);
+}
