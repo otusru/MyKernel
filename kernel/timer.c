e1218fa3ee165a3c90296976e107142b697b4698
otusru <otusru@mail.ru>
2025-05-30 10:43:46 +0300

    Update timer.c

--- kernel/timer.c
+++ kernel/timer.c
@@ -1,6 +1,12 @@
 #include "timer.h"
+#include "io.h"
+
+#define PIT_FREQ 1193180
+#define PIT_PORT 0x40
 
 void init_timer() {
-    // Заглушка: Настройка таймера
+    unsigned int divisor = PIT_FREQ / 100; // 100 Гц
+    outb(0x43, 0x36); // Command register
+    outb(PIT_PORT, divisor & 0xFF);
+    outb(PIT_PORT, (divisor >> 8) & 0xFF);
 }
-
