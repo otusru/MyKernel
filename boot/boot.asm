8148bc3bc52ed3b9996cfb7d98bfb1d2c373fb22
otusru <otusru@mail.ru>
2025-05-30 10:58:28 +0300

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,1 +1,17 @@
+// Загрузчик GRUB (Multiboot header)
 
+; boot/boot.asm
+[bits 32]
+section .multiboot
+align 4
+multiboot_header:
+    dd 0x1BADB002          ; magic number
+    dd 0x0                 ; flags
+    dd -0x1BADB002         ; checksum
+
+section .text
+global _start
+_start:
+    call kernel_main
+    cli
+    hlt
